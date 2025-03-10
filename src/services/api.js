ded0efd5d6f8a08ab80bab100e6b0fb704c5e900
bindk <sarakenny123@yahoo.com>
2025-03-11 05:53:43 +0700

    hotfix: force component using fixed prod url for now

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api/v1` : 'http://localhost:3000/api/v1';
+const API_URL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api/v1` : 'https://vsharing.bindk.ovh/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
--- src/services/cable.js
+++ src/services/cable.js
@@ -1,4 +1,4 @@
 import { createConsumer } from '@rails/actioncable';
 
-const cableUrl = process.env.REACT_APP_CABLE_URL || 'ws://localhost:3000/cable';
+const cableUrl = process.env.REACT_APP_CABLE_URL || 'wss://vsharing.bindk.ovh/cable';
 export const consumer = createConsumer(cableUrl);
